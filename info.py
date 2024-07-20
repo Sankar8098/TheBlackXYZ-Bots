ca83cd4fc416a17cab09cac1dc96e890974f1eac
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-20 11:05:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://theblackxyz-bots.onrender.com/")
+URL = environ.get("URL", "https://rapid-sun-f592.sankaraja8098.workers.dev/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
